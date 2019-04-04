4e17055d2b07aca0eb28091c8b14ddfd7bb7220b
Ricardo Herrera MAC <rherrerapetit@gmail.com>
2019-04-03 23:03:54 -0500

    Setting up UIImageView programatically

--- facefinder/ViewController.swift
+++ facefinder/ViewController.swift
@@ -14,6 +14,23 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        spinner.hidesWhenStopped = true
+        setupImageView()
+    }
+    
+    func setupImageView() {
+        guard let image = UIImage(named: "face") else { return }
+        
+        let imageView = UIImageView(image: image)
+        imageView.contentMode = .scaleAspectFit
+        
+        let scaledHeight = (view.frame.width / image.size.width) * image.size.height
+        
+        imageView.frame = CGRect(x: 0, y: 0, width: view.frame.width, height: scaledHeight)
+        imageView.layer.borderColor = UIColor.white.cgColor
+        imageView.layer.borderWidth = 2
+        view.addSubview(imageView)
+        
         spinner.startAnimating()
     }
 
